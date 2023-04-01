8d10125e47355844fe3908ffbda3c93d638bb321
hvcong <hvcong101201@gmail.com>
2023-04-01 12:37:22 +0700

    a

--- src/services/BillServices.js
+++ src/services/BillServices.js
@@ -95,6 +95,9 @@ const services = {
                       {
                         model: UnitType,
                       },
+                      {
+                        model: Product,
+                      },
                     ],
                   },
                 ],
