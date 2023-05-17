75917b4b83fa4fe91924ba760d66978231f3d725
hvcong <hvcong101201@gmail.com>
2023-05-17 10:37:01 +0700

    a

--- src/config/database.js
+++ src/config/database.js
@@ -1,42 +1,46 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config();
-// const sequelize = new Sequelize(process.env.DBNAME, process.env.USERNAME, process.env.PASSWORD, {
-//   host: process.env.DBHOST,
-//   port: 3306,
-//   dialect: "mysql",
-//   dialectOptions: {
-//     ssl: 'Amazon RDS'
-//   },
-//   logging: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000,
-//   },
-//   timezone: "+07:00",
-// });
-
-// module.exports = sequelize;
-
-const { Sequelize } = require ("sequelize");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 const sequelize = new Sequelize(
-    'minimarket',
-    'root',
-    null,
-    {
-        host:'localhost',
-        dialect: 'mysql',
-        logging: false,
-        pool:{
-            max:5,
-            min:0,
-            idle:10000,
-        }
-    }
-)
+  process.env.DBNAME,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.DBHOST,
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: "Amazon RDS",
+    },
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+    timezone: "+07:00",
+  }
+);
 
+module.exports = sequelize;
 
-module.exports = sequelize
+// const { Sequelize } = require ("sequelize");
+// const sequelize = new Sequelize(
+//     'minimarket',
+//     'root',
+//     null,
+//     {
+//         host:'localhost',
+//         dialect: 'mysql',
+//         logging: false,
+//         pool:{
+//             max:5,
+//             min:0,
+//             idle:10000,
+//         }
+//     }
+// )
+
+// module.exports = sequelize
 
 // online
 
