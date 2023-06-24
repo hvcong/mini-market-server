b508ce946f9296d0d9e868026e58fce54aaa0bf5
hvcong <hvcong101201@gmail.com>
2023-06-24 13:24:39 +0700

    a

--- src/routes/BillRoutes.js
+++ src/routes/BillRoutes.js
@@ -10,6 +10,7 @@ routes.get("/fail", controller.getPendingCancelBill);
 routes.put("/:id/update-type/:type", controller.updateType);
 routes.put("/:id/update-info", controller.updateInfo);
 routes.post("/from", controller.getSoldByDate);
+routes.delete("/:id/delete", controller.deleteById);
 routes.post("/customer", controller.getSoldByCustomer);
 routes.post("/retrieve", controller.getRetrieveBill);
 routes.get("/pending", controller.getPendingBills);
